e9770213d90e4953251ad65096d114faa231bee9
tendeanclaudior <tendeanclaudio1234@gmail.com>
2023-10-10 12:48:34 +0800

    first commit'

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateUI()
     }
-
+    
+    
+    @IBAction func choiceMode(_ sender: UIButton) {
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.getStorytitle()
+        choice1Button.setTitle(storyBrain.getchoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getchoice2(), for: .normal)
+    }
+    
 
 }
 
